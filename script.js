0bf2a8cb4e7f910fa9b638d54eb78024e42b7516
Alexander <alexanderm.kodehode@gmail.com>
2025-08-27 14:20:11 +0200

    Entries and sorting should work now

--- script.js
+++ script.js
@@ -118,3 +118,4 @@ document.querySelector("#delete-book").addEventListener("click", () => {
   deleteBook(bookId);
 });
 document.querySelector("#genreFilter").addEventListener("input", displayBooks);
+document.querySelector("#sortOption").addEventListener("change", displayBooks);
